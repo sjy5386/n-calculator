34ff69bdc5775272e209a64afa39b7f1a0f4c08e
Seokjin Yoon <sjy5386@gmail.com>
2022-04-17 21:46:33 +0900

    Update Person

--- src/main/java/com/sysbot32/ncalc/Person.java
+++ src/main/java/com/sysbot32/ncalc/Person.java
@@ -3,12 +3,15 @@ package com.sysbot32.ncalc;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @AllArgsConstructor
 @Getter
 @Setter
-@ToString
 public class Person {
     private String name;
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
