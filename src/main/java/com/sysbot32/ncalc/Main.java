f09c3171581f520ca7b32c45ca5a58fa73831b4f
Seokjin Yoon <sjy5386@gmail.com>
2022-04-17 21:02:11 +0900

    Add NCalculatorFrame

--- src/main/java/com/sysbot32/ncalc/Main.java
+++ src/main/java/com/sysbot32/ncalc/Main.java
@@ -1,9 +1,13 @@
 package com.sysbot32.ncalc;
 
+import com.sysbot32.ncalc.gui.NCalculatorFrame;
+
 public class Main {
     public static NCalculator nCalculator;
 
     public static void main(String[] args) {
         nCalculator = new NCalculator();
+
+        new NCalculatorFrame().setVisible(true);
     }
 }
---
+++ src/main/java/com/sysbot32/ncalc/gui/NCalculatorFrame.java
@@ -0,0 +1,9 @@
+package com.sysbot32.ncalc.gui;
+
+import javax.swing.*;
+
+public class NCalculatorFrame extends JFrame {
+    public NCalculatorFrame() {
+        super("N Calculator");
+    }
+}
