4173923a5578a64d881e940288dca6037c80987e
Seokjin Yoon <sjy5386@gmail.com>
2022-04-17 16:30:52 +0900

    Update Payment

--- src/main/java/com/sysbot32/ncalc/Payment.java
+++ src/main/java/com/sysbot32/ncalc/Payment.java
@@ -13,4 +13,23 @@ public class Payment {
     private Person payer;
     private Person payee;
     private BigInteger amount;
+
+    public void merge(Payment payment) {
+        if (payer == payment.payer && payee == payment.payee) {
+            amount = amount.add(payment.amount);
+        } else if (payer == payment.payee && payee == payment.payer) {
+            amount = amount.subtract(payment.amount);
+        }
+
+        if (amount.compareTo(BigInteger.ZERO) < 0) {
+            swap();
+        }
+    }
+
+    public void swap() {
+        Person tmp = payee;
+        payee = payer;
+        payer = tmp;
+        amount = amount.multiply(BigInteger.valueOf(-1));
+    }
 }
