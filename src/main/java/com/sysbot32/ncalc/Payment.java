22313c69e00541c1c2ea69de70b24ce5111f0059
Seokjin Yoon <sjy5386@gmail.com>
2022-04-17 16:03:31 +0900

    Add Payment

---
+++ src/main/java/com/sysbot32/ncalc/Payment.java
@@ -0,0 +1,17 @@
+package com.sysbot32.ncalc;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.math.BigInteger;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Payment {
+    private Person payer;
+    private Person payee;
+    private BigInteger amount;
+    private Round round;
+}
