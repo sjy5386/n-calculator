f92abe1b5810be48730e39a9ec0dba88fa8efb36
Seokjin Yoon <sjy5386@gmail.com>
2022-04-17 15:37:49 +0900

    Add Round

---
+++ src/main/java/com/sysbot32/ncalc/Round.java
@@ -0,0 +1,26 @@
+package com.sysbot32.ncalc;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class Round {
+    private String name;
+    private BigInteger amount;
+    private Person payer;
+    private final List<Person> participants;
+
+    public Round(String name, BigInteger amount, Person payer) {
+        this.name = name;
+        this.amount = amount;
+        this.payer = payer;
+        this.participants = new ArrayList<>();
+    }
+}
