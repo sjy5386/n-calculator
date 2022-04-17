e10ae632adcd44de3b0d269c7787bf6c97c681b0
Seokjin Yoon <sjy5386@gmail.com>
2022-04-17 15:23:28 +0900

    Add Person

---
+++ src/main/java/com/sysbot32/ncalc/Person.java
@@ -0,0 +1,14 @@
+package com.sysbot32.ncalc;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Person {
+    private String name;
+}
